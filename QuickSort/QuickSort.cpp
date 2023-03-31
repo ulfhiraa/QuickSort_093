46714c8bf5eb4b59c07a8c806cf49f9dce42d0cd
ulfhiraa <studyiraa@gmail.com>
2023-03-31 16:48:00 +0700

    langkah 2

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -43,4 +43,9 @@ void q_short(int low, int high)
 	if (low > high)  // langkah 1
 		return;
 
+	//Partition the list into two parts:
+//One containing elements less that or equal to pivot
+//Outher conntainning elements greather than pivot 
+
+	pivot = arr[low];  // langkah 2
 }
