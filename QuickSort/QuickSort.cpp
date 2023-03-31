6717f38335ae55c970156ce4cfbfa8083002ac94
ulfhiraa <studyiraa@gmail.com>
2023-03-31 17:15:17 +0700

    mencari element yang lebih besar dari pivot. langkah 5 dan langkah 6

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int arr[20] //array of integers to hold values
+int arr[20]; //array of integers to hold values
 int cmp_count = 0; // number of comparison
 int mov_count = 0; // number of data movements
 int n;
@@ -53,4 +53,8 @@ void q_short(int low, int high)
 	j = high; // langkah 4
 
 	while (i <= j) // langkah 10
+	{
+		// search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high)) // langkah 5
+	}
 }
