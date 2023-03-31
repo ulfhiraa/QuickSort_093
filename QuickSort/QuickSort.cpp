467bee016a3a82655d29b1b43a6bf62f27d5acc2
ulfhiraa <studyiraa@gmail.com>
2023-03-31 16:37:36 +0700

    prosedur untuk syarat dan menampilkan array

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -5,3 +5,20 @@ int arr[20] //array of integers to hold values
 int cmp_count = 0; // number of comparison
 int mov_count = 0; // number of data movements
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panjang element array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+	
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+}
